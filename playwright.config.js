30e7a8dfa8f8d1e3a4f5e06a5bbf2bbd493ac2cf
herbert <52478315+herbert1078@users.noreply.github.com>
2026-08-24 18:42:45 -0300

    chore: add playwright.config.js

---
+++ playwright.config.js
@@ -0,0 +1,17 @@
+// @ts-check
+const { defineConfig, devices } = require("@playwright/test");
+
+module.exports = defineConfig({
+  testDir: "./tests",
+  fullyParallel: true,
+  retries: process.env.CI ? 2 : 0,
+  reporter: [["list"], ["html", { open: "never" }]],
+  use: {
+    baseURL: "https://automationpratice.com.br",
+    trace: "on-first-retry",
+    screenshot: "only-on-failure",
+  },
+  projects: [
+    { name: "chromium", use: { ...devices["Desktop Chrome"] } },
+  ],
+});
